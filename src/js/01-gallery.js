4c535a1be9bf70d89660cb45ab15a4df5ea231dc
Chitorog Dan <bulgarud@gmail.com>
2024-06-29 13:16:29 +0300

    first commit

---
+++ src/js/01-gallery.js
@@ -0,0 +1,24 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+// console.log(galleryItems);
+const listEl = document.querySelector(".gallery");
+
+galleryItems.forEach(item => {
+    const listItem = document.createElement('li');
+    listItem.classList.add("gallery__item");
+    listItem.innerHTML = `
+        <a class="gallery__link" href="${item.original}">
+            <img class="gallery__image"
+                src="${item.preview}"
+                alt="${item.description}" />
+        </a>
+    `;
+    listEl.append(listItem);
+});
+
+// Initialize SimpleLightbox
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+    
